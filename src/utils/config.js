e22955e682b9533e433c620b891b3230ffde8852
Rahil Ansari <rahilansari261@gmail.com>
2024-11-18 18:03:55 +0530

    portfolio page bug fixed

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,4 @@
 export const BASE_URL = "http://127.0.0.1:8000/api/admin"
 // export const BASE_URL = "https://devvrat-new-backend.onrender.com/api/admin"
 // export const BASE_URL = "https://everythinge.nexprism.in/api/admin"
+export const IMAGE_BASE_URL = "http://127.0.0.1:8000/public/blog/images";
